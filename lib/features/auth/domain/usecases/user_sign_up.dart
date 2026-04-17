fe7e85eacae33f5c3fef238b9c4b1dac28bd0718
arshanishad <arshanishad5@gmail.com>
2026-04-17 14:38:17 +0530

    fixed the red warning error

--- lib/features/auth/domain/usecases/user_sign_up.dart
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -1,8 +1,12 @@
 import 'package:blog_app/core/theme/error/failure.dart';
 import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
 
 class UserSignUp implements Usecase<String, UserSignUpParams> {
+  final AuthRepository repository;
+
+  UserSignUp({required this.repository});
   @override
   Future<Either<Failure, String>> call(UserSignUpParams params) {
     throw UnimplementedError();
