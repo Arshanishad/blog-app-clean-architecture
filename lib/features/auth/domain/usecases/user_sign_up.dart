e7e3344f13092c06f2bb4cb2eaffdf50885db558
arshanishad <arshanishad5@gmail.com>
2026-05-01 15:19:22 +0530

    bloc add to these

--- lib/features/auth/domain/usecases/user_sign_up.dart
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -3,17 +3,16 @@ import 'package:blog_app/core/usecase/usecase.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
 
-
 // Usecase<ReturnType, InputType>
 /// In this case, the use case is for user sign-up, which returns a String (user ID) and takes UserSignUpParams as input.
 class UserSignUp implements Usecase<String, UserSignUpParams> {
-  final AuthRepository repository;
+  final AuthRepository authRepository;
 
-  UserSignUp({required this.repository});
+  UserSignUp({required this.authRepository});
   @override
   ///call  make this class callable like a function, allowing you to execute the sign-up logic by simply calling an instance of UserSignUp with the required parameters.
-  Future<Either<Failure, String>> call(UserSignUpParams params) {
-  return repository.signUpWithEmailAndPassword(
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+  return await  authRepository.signUpWithEmailAndPassword(
     email: params.email,
     password: params.password,
     name:params.name,
