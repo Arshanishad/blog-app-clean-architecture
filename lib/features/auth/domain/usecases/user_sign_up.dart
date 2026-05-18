7ccb24e674ee8c86157b12beec89aac595c8602c
arshanishad <arshanishad5@gmail.com>
2026-05-18 10:55:40 +0530

    domain layer user signup funtion return change to user

--- lib/features/auth/domain/usecases/user_sign_up.dart
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -1,17 +1,18 @@
 import 'package:blog_app/core/error/failure.dart';
 import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/auth/domain/entities/user.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
 
 // Usecase<ReturnType, InputType>
 /// In this case, the use case is for user sign-up, which returns a String (user ID) and takes UserSignUpParams as input.
-class UserSignUp implements Usecase<String, UserSignUpParams> {
+class UserSignUp implements Usecase<User, UserSignUpParams> {
   final AuthRepository authRepository;
 
   UserSignUp({required this.authRepository});
   @override
   ///call  make this class callable like a function, allowing you to execute the sign-up logic by simply calling an instance of UserSignUp with the required parameters.
-  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+  Future<Either<Failure, User>> call(UserSignUpParams params) async {
   return await  authRepository.signUpWithEmailAndPassword(
     email: params.email,
     password: params.password,
